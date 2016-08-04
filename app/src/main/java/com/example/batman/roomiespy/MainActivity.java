e7dd573bdb883dc0ba81835df94c512a0c67d756
ashok942 <ashok942@hotmail.com>
2016-08-03 23:53:41 -0700

    Changes to update the users current location dynamically.

--- app/src/main/java/com/example/batman/roomiespy/MainActivity.java
+++ app/src/main/java/com/example/batman/roomiespy/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.batman.roomiespy;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,22 +18,22 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,LocationListener{
 
     private MapView mapView;
     private GoogleMap googleMap;
-
+    private LatLng locate;
+    private LocationManager locationManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         mapView =(MapView) findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
-
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
         mapView.getMapAsync(this);
 
     }
@@ -37,17 +41,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
-        this.googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(37.3335423, -121.9132912))
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-        Log.d("onMap","Hey");
+
+        //this.googleMap.addMarker(new MarkerOptions().position(locate).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
         this.googleMap.setMyLocationEnabled(true);
+
         this.googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-        MapsInitializer.initialize(getApplicationContext());
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(new LatLng(37.3335423, -121.9132912)).zoom(15).bearing(90).tilt(40).build();
-        this.googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+
     }
+
 
     @Override
     public void onResume() {
@@ -65,5 +68,31 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onPause() {
         super.onPause();
         mapView.onPause();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        locate = new LatLng(location.getLatitude(),location.getLongitude());
+
+        MapsInitializer.initialize(getApplicationContext());
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(locate).zoom(15).bearing(90).tilt(40).build();
+        this.googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
