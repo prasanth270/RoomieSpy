a991847ec60846846bb2a41ee160e3a12065eaba
Prasanth Ramineni <prasanth270@gmail.com>
2016-08-03 22:57:15 -0700

    Adding Google Maps API

--- app/src/main/java/com/example/batman/roomiespy/MainActivity.java
+++ app/src/main/java/com/example/batman/roomiespy/MainActivity.java
@@ -2,12 +2,68 @@ package com.example.batman.roomiespy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private MapView mapView;
+    private GoogleMap googleMap;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mapView =(MapView) findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        this.googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(37.3335423, -121.9132912))
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        Log.d("onMap","Hey");
+        this.googleMap.setMyLocationEnabled(true);
+        this.googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        MapsInitializer.initialize(getApplicationContext());
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(37.3335423, -121.9132912)).zoom(15).bearing(90).tilt(40).build();
+        this.googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
     }
 }
